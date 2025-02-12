eaff4acf0793949e8cae312f3eae3765135d2fa5
surdav <31798966+surdav@users.noreply.github.com>
2025-02-13 00:10:22 +0100

    Refactoring unit test and adding test-questions.csv

--- hw01-xml-config/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw01-xml-config/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -3,13 +3,16 @@ package ru.otus.hw.dao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class CsvQuestionDaoTest {
@@ -24,6 +27,22 @@ class CsvQuestionDaoTest {
 
         // Create a new instance of CsvQuestionDao
         csvQuestionDao = new CsvQuestionDao(testFileNameProvider);
+    }
+
+    @Test
+    void findAll_ShouldReturnListOfQuestions_WhenCsvIsValid() {
+        // Arrange
+        String testFileName = "test-questions.csv";
+        when(testFileNameProvider.getTestFileName()).thenReturn(testFileName);
+
+        // Act
+        List<Question> questions = csvQuestionDao.findAll();
+
+        // Assert
+        assertThat(questions).hasSize(3);
+        assertThat(questions.get(0).text()).isEqualTo("Is there life on Mars?");
+        assertThat(questions.get(1).text()).isEqualTo("How should resources be loaded form jar in Java?");
+        assertThat(questions.get(2).text()).isEqualTo("Which option is a good way to handle the exception?");
     }
 
     @Test
@@ -46,7 +65,7 @@ class CsvQuestionDaoTest {
     @Test
     void findAll_ShouldThrowQuestionReadException_WhenCsvIsMalformed() {
         // Arrange
-        String testFileName = "malformed-questions.csv";
+        String testFileName = "malformed-test-questions.csv";
         String csvContent = """
                 # Header to skip
                 Only one column here;MalformedAnswer
