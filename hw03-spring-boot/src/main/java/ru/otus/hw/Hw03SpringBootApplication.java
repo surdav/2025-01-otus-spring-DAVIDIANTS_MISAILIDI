03fa9fc61978115166a16de0b2ce92fb89cecfbf
surdav <31798966+surdav@users.noreply.github.com>
2025-03-09 15:50:41 +0100

    refactor

--- hw03-spring-boot/src/main/java/ru/otus/hw/Hw03SpringBootApplication.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/Hw03SpringBootApplication.java
@@ -3,9 +3,12 @@ package ru.otus.hw;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.service.TestRunnerService;
 
+@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class Hw03SpringBootApplication {
 
--- hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,10 +2,8 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 @ConfigurationProperties(prefix = "test") // Binds the “test” prefix from application.yml
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
