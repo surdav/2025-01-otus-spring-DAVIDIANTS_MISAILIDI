f57ecf2ce4e36f8d8f2d1c9b8c5eb9baa0a7d49d
surdav <31798966+surdav@users.noreply.github.com>
2025-04-13 20:44:12 +0200

    refactor style

--- hw-11-jpql/src/main/java/ru/otus/hw/models/Book.java
+++ hw-11-jpql/src/main/java/ru/otus/hw/models/Book.java
@@ -1,6 +1,18 @@
 package ru.otus.hw.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 
 import lombok.Getter;
 import lombok.Setter;
